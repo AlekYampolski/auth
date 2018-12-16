e8f455e2f119a433a3fe1d4a1885c758d5c0dca3
Aleksander Yampolski <al.yampolski@gmail.com>
2018-12-16 22:31:49 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+
+}
